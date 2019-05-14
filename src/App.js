2bc9f64b56dac53696e46716d812209528d04125
smaster0517 <smaster0517@gmail.com>
2019-05-14 20:34:16 -0300

    rough design with animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import coin from "./images/coin.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="screen">
+        <div class="x1">
+          <div class="cloud" />
+        </div>
+        <div class="x2">
+          <div class="cloud" />
+        </div>
+        <div class="x3">
+          <div class="cloud" />
+        </div>
+        <div class="x4">
+          <div class="cloud" />
+        </div>
+        <div class="x5">
+          <div class="cloud" />
+        </div>
+        <div className="dashboard">+ 3243500 Bits<br/>132 Hops</div>
+        
+        <div className="ballBox">
+          <img src={coin} alt="coin" className="ball" />
+        </div>
+        <div className="mainBox">
+          <div className="currentPlate">
+            <div className="plateText">+1000 Bits!</div>
+          </div>
+          <div className="nextPlate">
+            <div className="plateText">x 10</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
